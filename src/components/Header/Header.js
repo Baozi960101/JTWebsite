a242cf9210adf45ae519433acde1a1ca5c0a1b0c
John <Baozi960101@gmail.com>
2021-08-17 18:39:45 +0800

    2021/08/17

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { ReactComponent as downArrow } from "../../images/downArrow.svg";
 import jianTongLogo from "../../images/jianTongLogo.png";
 import upArrow from "../../images/upArrow.svg";
+import menu from "../../images/menu.svg";
 import { Link } from "react-router-dom";
 
 const HeaderBox = styled.div`
@@ -15,6 +16,86 @@ const HeaderBox = styled.div`
   font-weight: 600;
   box-sizing: border-box;
   background-color: white;
+
+  @media screen and (max-width: 599px) {
+    align-items: center;
+    padding: 0 30px 0 20px;
+  }
+
+  @media screen and (min-width: 600px) and (max-width: 1050px) {
+    padding: 20px 30px;
+  }
+`;
+
+const HeaderRwdMenuBottom = styled.img`
+  display: none;
+  width: 40px;
+  height: 40px;
+
+  @media screen and (max-width: 599px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const MenuBox = styled.div`
+  display: flex;
+  width: 0;
+  height: 100%;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  transition: width 0.3s ease-in-out;
+  z-index: 6;
+
+  ${(props) => props.$move && `width:100%`}
+`;
+
+const MenuLeft = styled.div`
+  width: 80%;
+  height: 100%;
+  background-color: white;
+`;
+
+const MenuLeftTopHeader = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: black;
+`;
+
+const MenuLeftMainBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 30px 30px;
+`;
+
+const MenuLeftSubTitle = styled(Link)`
+  width: 80%;
+  height: 50px;
+  text-decoration: none;
+  font-size: 20px;
+  color: black;
+  font-weight: 600;
+`;
+
+const MenuLeftSubTitleProduct = styled(Link)`
+  width: 80%;
+  height: 40px;
+  text-decoration: none;
+  font-size: 16px;
+  color: black;
+  font-weight: 500;
+  padding-left: 30px;
+`;
+
+const MenuRight = styled.div`
+  width: 20%;
+  height: 100%;
+  background-color: black;
+  opacity: 0.8;
 `;
 
 const HeaderMainBox = styled(Link)`
@@ -41,6 +122,10 @@ const HeaderMainTitle = styled.div`
   height: 60%;
   padding-left: 13px;
   font-size: 20px;
+
+  @media screen and (min-width: 600px) and (max-width: 1050px) {
+    font-size: 18px;
+  }
 `;
 
 const HeaderMainSubTitle = styled.div`
@@ -48,6 +133,10 @@ const HeaderMainSubTitle = styled.div`
   padding-left: 13px;
   font-size: 14px;
   color: gray;
+
+  @media screen and (min-width: 600px) and (max-width: 1050px) {
+    font-size: 12px;
+  }
 `;
 
 const HeaderSubBox = styled.div`
@@ -58,6 +147,10 @@ const HeaderSubBox = styled.div`
   height: 100%;
   font-size: 20px;
   font-weight: 600;
+
+  @media screen and (max-width: 599px) {
+    display: none;
+  }
 `;
 const HeaderSubTitle = styled(Link)`
   position: relative;
@@ -68,6 +161,10 @@ const HeaderSubTitle = styled(Link)`
   :hover {
     color: red;
     fill: red;
+  }
+
+  @media screen and (min-width: 600px) and (max-width: 1050px) {
+    font-size: 18px;
   }
 `;
 
@@ -125,11 +222,17 @@ const HeaderDownArrow = styled(downArrow)`
   padding-top: 3px;
   width: 23px;
   height: 23px;
+
+  @media screen and (min-width: 600px) and (max-width: 1050px) {
+    width: 18px;
+    height: 18px;
+  }
 `;
 
 export default function Header() {
   const [dropDown, setDropDown] = useState(false);
   const [topBottomArise, setTopBottomArise] = useState(false);
+  const [menuOn, setMenuOn] = useState(false);
 
   useEffect(() => {
     window.onscroll = function () {
@@ -161,8 +264,49 @@ export default function Header() {
     setDropDown(false);
   }
 
+  function HandleMenu() {
+    if (menuOn) {
+      setMenuOn(false);
+    } else {
+      setMenuOn(true);
+    }
+  }
+
   return (
     <>
+      <MenuBox $move={menuOn}>
+        <MenuLeft>
+          <MenuLeftTopHeader />
+          <MenuLeftMainBox>
+            <MenuLeftSubTitle>產品介紹</MenuLeftSubTitle>
+            <MenuLeftSubTitleProduct to="/argus" onClick={HandleMenu}>
+              Argus
+            </MenuLeftSubTitleProduct>
+            <MenuLeftSubTitleProduct to="/venus" onClick={HandleMenu}>
+              Venus
+            </MenuLeftSubTitleProduct>
+            <MenuLeftSubTitleProduct to="/mars" onClick={HandleMenu}>
+              Mars
+            </MenuLeftSubTitleProduct>
+            <MenuLeftSubTitleProduct to="/z-max" onClick={HandleMenu}>
+              Z-Max
+            </MenuLeftSubTitleProduct>
+            <MenuLeftSubTitle to="/resultssharing" onClick={HandleMenu}>
+              成果分享
+            </MenuLeftSubTitle>
+            <MenuLeftSubTitle to="/partners" onClick={HandleMenu}>
+              合作廠商
+            </MenuLeftSubTitle>
+            <MenuLeftSubTitle to="/recruitment" onClick={HandleMenu}>
+              人才招聘
+            </MenuLeftSubTitle>
+            <MenuLeftSubTitle to="/contact" onClick={HandleMenu}>
+              聯絡我們
+            </MenuLeftSubTitle>
+          </MenuLeftMainBox>
+        </MenuLeft>
+        <MenuRight onClick={HandleMenu}></MenuRight>
+      </MenuBox>
       <HeaderBox>
         <HeaderMainBox to="/">
           <HeaderLogo src={jianTongLogo} />
@@ -187,8 +331,9 @@ export default function Header() {
           <HeaderSubTitle to="/resultssharing">成果分享</HeaderSubTitle>
           <HeaderSubTitle to="/partners">合作廠商</HeaderSubTitle>
           <HeaderSubTitle to="/recruitment">人才招聘</HeaderSubTitle>
-          <HeaderSubTitle to="/Contact">聯絡我們</HeaderSubTitle>
+          <HeaderSubTitle to="/contact">聯絡我們</HeaderSubTitle>
         </HeaderSubBox>
+        <HeaderRwdMenuBottom onClick={HandleMenu} src={menu} />
       </HeaderBox>
       {topBottomArise && <TopBottom onClick={topFunction} src={upArrow} />}
     </>
--- src/page/product/Argus/Argus.js
+++ src/page/product/Argus/Argus.js
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import Argus01 from "../../../images/Argus1.png";
+import Argus02 from "../../../images/Argus2.png";
+import Argus03 from "../../../images/Argus3.png";
+import Argus04 from "../../../images/Argus4.png";
+import Argus05 from "../../../images/Argus5.png";
+
+const ArgusBox = styled.img`
+  display: flex;
+  max-width: 100%;
+  height: auto;
+`;
 
 export default function Argus() {
   return (
     <>
-      <div>我是阿古斯</div>
+      <ArgusBox src={Argus01} />
+      <ArgusBox src={Argus02} />
+      <ArgusBox src={Argus03} />
+      <ArgusBox src={Argus04} />
+      <ArgusBox src={Argus05} />
     </>
   );
 }
